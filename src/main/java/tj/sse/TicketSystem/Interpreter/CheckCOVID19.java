909b25e583b22b24b306f67fd24e9004bc1a3ce7
Sherlock White <1172940427@qq.com>
2021-10-18 20:08:30 +0800

    修改为英文/调整目录结构

--- src/main/java/tj/sse/TicketSystem/Interpreter/CheckCOVID19.java
+++ src/main/java/tj/sse/TicketSystem/Interpreter/CheckCOVID19.java
@@ -4,19 +4,19 @@ import java.util.*;
 public class CheckCOVID19 {
     /*文法规则
       <expression> ::= <address>的<name>
-      <address> ::= 北京市通州区 | 洛杉矶好莱坞 | 佛罗里达州奥兰多市 | 大阪市此花区 | 新加坡
-      <name> ::= 北京环球主题公园 | 好莱坞环球影城 | 奥兰多环球影城 | 日本环球影城 | 新加坡环球影城
+      <address> ::= Beijing | Los Angeles | Florida | Osaka | Singapore
+      <name> ::= Universal Studios
     */
     public static void main(String[] args) {
         Context condition = new Context();
         System.out.println("--------------");
-        System.out.println("北京市通州区的北京环球主题公园：#1");
-        System.out.println("洛杉矶好莱坞的好莱坞环球影城：#2");
-        System.out.println("佛罗里达州奥兰多市的奥兰多环球影城：#3");
-        System.out.println("大阪市此花区的日本环球影城：#4");
-        System.out.println("新加坡的新加坡环球影城：#5");
-        System.out.println("退出：#exit");
-        System.out.println("请选择购票的环球影城：");
+        System.out.println("Universal Studios in Beijing：#1");
+        System.out.println("Universal Studios in Los Angeles：#2");
+        System.out.println("Universal Studios in Florida：#3");
+        System.out.println("Universal Studios in Osaka：#4");
+        System.out.println("Universal Studios in Singapore：#5");
+        System.out.println("exit：#exit");
+        System.out.println("please choose the Universal Studio to buy tickets:");
 
         while(true){
             //接受输入的指令
@@ -25,68 +25,25 @@ public class CheckCOVID19 {
                 break;
             }
             else if (Objects.equals(cmd,"#1")){
-                condition.freeRide("北京市通州区的北京环球主题公园");
+                condition.freeRide("Universal Studios in Beijing");
             }
             else if (Objects.equals(cmd,"#2")){
-                condition.freeRide("洛杉矶好莱坞的好莱坞环球影城");
+                condition.freeRide("Universal Studios in Los Angeles");
             }
             else if (Objects.equals(cmd,"#3")){
-                condition.freeRide("佛罗里达州奥兰多市的奥兰多环球影城");
+                condition.freeRide("Universal Studios in Florida");
             }
             else if (Objects.equals(cmd,"#4")){
-                condition.freeRide("大阪市此花区的日本环球影城");
+                condition.freeRide("Universal Studios in Osaka");
 
             }
             else if (Objects.equals(cmd,"#5")){
-                condition.freeRide("新加坡的新加坡环球影城");
+                condition.freeRide("Universal Studios in Singapore");
             }
             else{
-                System.out.println("请输入正确的指令");
+                System.out.println("please input right command");
             }
         }
     }
 }
 
-//抽象表达式类
-interface Expression {
-    public boolean interpret(String info);
-}
-//终结符表达式类
-class TerminalExpression implements Expression {
-    private final Set<String> set = new HashSet<String>();
-    public TerminalExpression(String[] data) {
-        Collections.addAll(set, data);
-    }
-    public boolean interpret(String info) {
-        return set.contains(info);
-    }
-}
-//非终结符表达式类
-class AndExpression implements Expression {
-    private Expression address = null;
-    private Expression name = null;
-    public AndExpression(Expression address, Expression name) {
-        this.address = address;
-        this.name = name;
-    }
-    public boolean interpret(String info) {
-        String[] s = info.split("的");
-        return address.interpret(s[0]) && name.interpret(s[1]);
-    }
-}
-//环境类
-class Context {
-    private String[] addresses = {"洛杉矶好莱坞","佛罗里达州奥兰多市","大阪市此花区","新加坡"};
-    private String[] names = {"好莱坞环球影城","奥兰多环球影城","日本环球影城","新加坡环球影城"};
-    private Expression addressName;
-    public Context() {
-        Expression address = new TerminalExpression(addresses);
-        Expression name = new TerminalExpression(names);
-        addressName = new AndExpression(address, name);
-    }
-    public void freeRide(String info) {
-        boolean ok = addressName.interpret(info);
-        if (!ok) System.out.println(info + "是低风险区域，可以购票");
-        else System.out.println(info + "是中高风险区域，暂不提供购票功能");
-    }
-}
--- src/main/java/tj/sse/TicketSystem/Memento/InfoRecovery.java
+++ src/main/java/tj/sse/TicketSystem/Memento/InfoRecovery.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class InfoRecovery {
     public static void main(String[] args) {
         System.out.println("--------------");
-        System.out.println("输入信息：#input");
-        System.out.println("修改信息：#change");
-        System.out.println("回退至最近一次修改：#rollback");
-        System.out.println("退出购票：#exit");
-        System.out.println("请输入指令：");
+        System.out.println("input information：#input");
+        System.out.println("change information：#change");
+        System.out.println("roll back information：#rollback");
+        System.out.println("exit this pard：#exit");
+        System.out.println("please input command：");
 
         Originator or = new Originator();
         Caretaker cr = new Caretaker();
@@ -17,69 +17,28 @@ public class InfoRecovery {
             //接受输入的指令
             String cmd = new Scanner(System.in).next();
             if(Objects.equals(cmd,"#input")){
-                System.out.println("请输入姓名：");
+                System.out.println("please input your name：");
                 String name = new Scanner(System.in).next();
                 or.setState(name);
-                System.out.println("初始姓名:" + or.getState());
+                System.out.println("original name:" + or.getState());
                 cr.setMemento(or.createMemento()); //保存状态
             }
             else if(Objects.equals(cmd,"#change")){
-                System.out.println("请输入新的姓名：");
+                System.out.println("please input your new name：");
                 String name = new Scanner(System.in).next();
                 or.setState(name);
-                System.out.println("新的姓名:" + or.getState());
+                System.out.println("current name:" + or.getState());
             }
             else if(Objects.equals(cmd,"#rollback")){
                 or.restoreMemento(cr.getMemento()); //恢复状态
-                System.out.println("恢复姓名:" + or.getState());
+                System.out.println("current name:" + or.getState());
             }
             else if(Objects.equals(cmd,"#exit")){
                 break;
             }
             else{
-                System.out.println("请输入有效的指令");
+                System.out.println("please enter a valid");
             }
         }
-    }
-}
-
-//备忘录
-class Memento {
-    private String state;
-    public Memento(String state) {
-        this.state = state;
-
-    }
-    public void setState(String state) {
-        this.state = state;
-    }
-    public String getState() {
-        return state;
-    }
-}
-//发起人
-class Originator {
-    private String state;
-    public void setState(String state) {
-        this.state = state;
-    }
-    public String getState() {
-        return state;
-    }
-    public Memento createMemento() {
-        return new Memento(state);
-    }
-    public void restoreMemento(Memento m) {
-        this.setState(m.getState());
-    }
-}
-//管理者
-class Caretaker {
-    private Memento memento;
-    public void setMemento(Memento m) {
-        memento = m;
-    }
-    public Memento getMemento() {
-        return memento;
     }
 }
