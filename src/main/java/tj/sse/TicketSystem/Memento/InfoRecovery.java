9418688f12bc8aa46c5ea6990ece6d76693f38a8
Sherlock White <1172940427@qq.com>
2021-10-18 16:30:12 +0800

    备忘录的输入调整完成

--- src/main/java/tj/sse/TicketSystem/Memento/InfoRecovery.java
+++ src/main/java/tj/sse/TicketSystem/Memento/InfoRecovery.java
@@ -1,16 +1,42 @@
 package tj.sse.TicketSystem.Memento;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class InfoRecovery {
     public static void main(String[] args) {
+        System.out.println("--------------");
+        System.out.println("输入信息：#input");
+        System.out.println("修改信息：#change");
+        System.out.println("回退至最近一次修改：#rollback");
+        System.out.println("退出购票：#exit");
+        System.out.println("请输入指令：");
+
         Originator or = new Originator();
         Caretaker cr = new Caretaker();
-        or.setState("小红创建了订单S0");
-        System.out.println("初始状态:" + or.getState());
-        cr.setMemento(or.createMemento()); //保存状态
-        or.setState("小红取消了订单S0");
-        System.out.println("新的状态:" + or.getState());
-        or.restoreMemento(cr.getMemento()); //恢复状态
-        System.out.println("恢复状态:" + or.getState());
+        while(true){
+            //接受输入的指令
+            String cmd = new Scanner(System.in).next();
+            if(Objects.equals(cmd,"#input")){
+                System.out.println("请输入姓名：");
+                String name = new Scanner(System.in).next();
+                or.setState(name);
+                System.out.println("初始姓名:" + or.getState());
+                cr.setMemento(or.createMemento()); //保存状态
+            }
+            else if(Objects.equals(cmd,"#change")){
+                System.out.println("请输入新的姓名：");
+                String name = new Scanner(System.in).next();
+                or.setState(name);
+                System.out.println("新的姓名:" + or.getState());
+            }
+            else if(Objects.equals(cmd,"#rollback")){
+                or.restoreMemento(cr.getMemento()); //恢复状态
+                System.out.println("恢复姓名:" + or.getState());
+            }
+            else if(Objects.equals(cmd,"#exit")){
+                break;
+            }
+        }
     }
 }
 
