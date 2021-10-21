4a92fb9d72d7b643a3332f132dd89ee37297db14
lzr <2591707133@qq.com>
2021-10-21 20:09:07 +0800

    templatepattern class

--- src/main/java/tj/sse/playItems/Route/strategy/routePlanning.java
+++ src/main/java/tj/sse/playItems/Route/strategy/routePlanning.java
@@ -10,40 +10,8 @@ public abstract class routePlanning {
     public abstract String[] provideSuggestions();
     public static void main( String[] args )
     {
-        templatemethod tm = new strategyA();
-        String[] tmp = tm.provideSuggestions();
-        //System.out.println( tmp[1]);
+        routePlanning tm = new strategyA();
+        //String[] tmp = tm.provideSuggestions();
     }
 }
 
-/**
- * 策略A
- */
-class strategyA extends routePlanning {
-
-    @Override
-    public String[] provideSuggestions() {
-        String[] names = {"PotionClass","DecepticoasterGame","JurassicFlyersCoasterGame","WandGame","RobotGame"};
-        return names;
-    }
-
-}
-
-/**
- * 策略B
- */
-class strategyB extends routePlanning {
-
-    public String[] provideSuggestions() {
-        String[] names = {"WandGame","PotionClass","JurassicFlyersCoasterGame","DecepticoasterGame","RobotGame"};
-        return names;
-    }
-}
-
-class strategyC extends routePlanning {
-
-    public String[] provideSuggestions() {
-        String[] names = {"JurassicFlyersCoasterGame","WandGame","PotionClass","RobotGame","DecepticoasterGame"};
-        return names;
-    }
-}
---
+++ src/main/java/tj/sse/playItems/Template/JurassicFlyersCoaster.java
@@ -0,0 +1,8 @@
+package tj.sse.playItems.Template;
+
+class JurassicFlyersCoaster extends templateMethodAbstractClass {
+    public void startRollerCoaster() {
+        System.out.println("Start JurassicFlyersCoaster.");
+    }
+
+}
