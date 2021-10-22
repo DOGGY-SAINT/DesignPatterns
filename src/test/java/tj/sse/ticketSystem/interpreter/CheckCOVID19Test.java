e24f24b4fa3e80c21c6fa9c55af6517d6e885895
Sherlock White <1172940427@qq.com>
2021-10-22 17:08:41 +0800

    添加解释器的test

--- src/test/java/tj/sse/ticketSystem/interpreter/CheckCOVID19Test.java
+++ src/test/java/tj/sse/ticketSystem/interpreter/CheckCOVID19Test.java
@@ -3,15 +3,55 @@ package tj.sse.ticketSystem.interpreter;
 import org.junit.jupiter.api.Test;
 import tj.sse.AppTest;
 
+import java.util.Objects;
+
 public class CheckCOVID19Test {
     @Test
     public void checkCOVID19Test(){
         System.out.println("--------------- [Test] CheckCOVID19 ---------------");
-        if(true){
+        if(AppTest.input){
+            Context condition = new Context();
+            while(true){
+                System.out.println("Universal Studios in Beijing：#1");
+                System.out.println("Universal Studios in Los Angeles：#2");
+                System.out.println("Universal Studios in Florida：#3");
+                System.out.println("Universal Studios in Osaka：#4");
+                System.out.println("Universal Studios in Singapore：#5");
+                System.out.println("exit：#exit");
+                System.out.println("please choose the Universal Studio to buy tickets:");
+                //接受输入的指令
+                String cmd= AppTest.scanner.next();
+                if(Objects.equals(cmd, "#exit")){
+                    break;
+                }
+                else if (Objects.equals(cmd,"#1")){
+                    condition.freeRide("Universal Studios in Beijing");
+                }
+                else if (Objects.equals(cmd,"#2")){
+                    condition.freeRide("Universal Studios in Los Angeles");
+                }
+                else if (Objects.equals(cmd,"#3")){
+                    condition.freeRide("Universal Studios in Florida");
+                }
+                else if (Objects.equals(cmd,"#4")){
+                    condition.freeRide("Universal Studios in Osaka");
 
+                }
+                else if (Objects.equals(cmd,"#5")){
+                    condition.freeRide("Universal Studios in Singapore");
+                }
+                else{
+                    System.out.println("please input right command.");
+                }
+            }
         }
         else{
-
+            Context condition = new Context();
+            condition.freeRide("Universal Studios in Beijing");
+            condition.freeRide("Universal Studios in Los Angeles");
+            condition.freeRide("Universal Studios in Florida");
+            condition.freeRide("Universal Studios in Osaka");
+            condition.freeRide("Universal Studios in Singapore");
         }
         System.out.println("--------------------- [End]  ----------------------");
     }
