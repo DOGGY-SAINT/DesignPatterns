6db978678a972b7dbded099e03147019e853f15d
DOGGY-SAINT <1123117528@qq.com>
2021-11-01 16:43:18 +0800

    小吃测试加上了循环，修复了饮料的一个bug

--- src/test/java/tj/sse/snackshop/SnackshopTest.java
+++ src/test/java/tj/sse/snackshop/SnackshopTest.java
@@ -22,75 +22,78 @@ public class SnackshopTest {
     public void SnackshopTest(){
         System.out.println("------------- [Test] Snackshop -------------");
         if(AppTest.input){
-            System.out.println("What do you want to buy?");
-            System.out.println("1.Drinks");
-            System.out.println("2.Icecream");
-            System.out.println("3.MilkTea");
-            System.out.println("4.Nothing to buy");
-            System.out.println("Select the above serial number:");
-            int i= AppTest.scanner.nextInt();
-            if(i==1){
-                System.out.println("------------ [Test] Factory Method ------------");
-                System.out.println("Do you want Fanta or Cola?:");
-                String str= AppTest.scanner.next();
-                DrinksSeller seller=new DrinksSeller();
-                Drinks mt;
-                mt=seller.getDrinks(str);
-                System.out.println(mt.getInf());
-                System.out.println("------------------- [End] --------------------");
-            }
-            else if(i==2){
-                System.out.println("--------------- [Test] Adapter ---------------");
-                System.out.println("What flavor of ice cream do you want(blueberries, chocolate, or something else):");
-                String str= AppTest.scanner.next();
-                IcecreamMachine im=new IcecreamMachine();
-                System.out.println(im.getIcecream(str));
-                System.out.println("------------------- [End] --------------------");
-            }
-            else if(i==3) {
-                System.out.println("--------- [Test] Bridge and Builder ----------");
-                System.out.println("MilkTea is as follows:");
-                System.out.println("1.PearlMilkTea");
-                System.out.println("2.CocountMilkTea");
-                System.out.println("3.Free set milk tea");
-                System.out.println("4.I don't drink milk tea!");
-                System.out.println("What kind of milk tea do you want:");
-                int choose = AppTest.scanner.nextInt();
-
-                MilkTeaMachine machine = new MilkTeaMachine();
-                MilkTea mt;
-                if (choose == 1) {
-                    mt = machine.getMilkTea("PearlMilkTea");
-                    System.out.println("Your MilkTea:" + mt.getInf());
-                } else if (choose == 2) {
-                    mt = machine.getMilkTea("CocountMilkTea");
-                    System.out.println("Your MilkTea:" + mt.getInf());
-                } else if (choose == 3) {
-                    System.out.println("Ice:Hot/Cold");
-                    System.out.println("Sweetness:HalfSuger/LittleSuger");
-                    System.out.println("Item:Cocount/Pearl");
-                    System.out.println("This is an example:Hot;HalfSuger;Cocount");
-                    System.out.println("Please enter your milk tea type according to the above format:");
-                    String str = AppTest.scanner.next();
-                    mt = machine.getMilkTea(str);
-                    if (mt == null)
-                        System.out.println("Wrong input");
+            while(true){
+                System.out.println("What do you want to buy?");
+                System.out.println("1.Drinks");
+                System.out.println("2.Icecream");
+                System.out.println("3.MilkTea");
+                System.out.println("4.Nothing to buy");
+                System.out.println("Select the above serial number:");
+                int i= AppTest.scanner.nextInt();
+                if(i==1){
+                    System.out.println("------------ [Test] Factory Method ------------");
+                    System.out.println("Do you want Fanta or Cola?:");
+                    String str= AppTest.scanner.next();
+                    DrinksSeller seller=new DrinksSeller();
+                    Drinks mt;
+                    mt=seller.getDrinks(str);
+                    if(mt==null)
+                        System.out.println(str+" not exist");
                     else
+                        System.out.println(mt.getInf());
+                }
+                else if(i==2){
+                    System.out.println("--------------- [Test] Adapter ---------------");
+                    System.out.println("What flavor of ice cream do you want(blueberries, chocolate, or something else):");
+                    String str= AppTest.scanner.next();
+                    IcecreamMachine im=new IcecreamMachine();
+                    System.out.println(im.getIcecream(str));
+                }
+                else if(i==3) {
+                    System.out.println("--------- [Test] Bridge and Builder ----------");
+                    System.out.println("MilkTea is as follows:");
+                    System.out.println("1.PearlMilkTea");
+                    System.out.println("2.CocountMilkTea");
+                    System.out.println("3.Free set milk tea");
+                    System.out.println("4.I don't drink milk tea!");
+                    System.out.println("What kind of milk tea do you want:");
+                    int choose = AppTest.scanner.nextInt();
+
+                    MilkTeaMachine machine = new MilkTeaMachine();
+                    MilkTea mt;
+                    if (choose == 1) {
+                        mt = machine.getMilkTea("PearlMilkTea");
                         System.out.println("Your MilkTea:" + mt.getInf());
-                } else if (choose == 4) {
-                    System.out.println("Send you a cup of milk tea, maybe you will become addicted!");
-                    mt = machine.getMilkTea("PearlMilkTea");
-                    System.out.println("Your MilkTea:" + mt.getInf());
-                } else {
+                    } else if (choose == 2) {
+                        mt = machine.getMilkTea("CocountMilkTea");
+                        System.out.println("Your MilkTea:" + mt.getInf());
+                    } else if (choose == 3) {
+                        System.out.println("Ice:Hot/Cold");
+                        System.out.println("Sweetness:HalfSuger/LittleSuger");
+                        System.out.println("Item:Cocount/Pearl");
+                        System.out.println("This is an example:Hot;HalfSuger;Cocount");
+                        System.out.println("Please enter your milk tea type according to the above format:");
+                        String str = AppTest.scanner.next();
+                        mt = machine.getMilkTea(str);
+                        if (mt == null)
+                            System.out.println("Wrong input");
+                        else
+                            System.out.println("Your MilkTea:" + mt.getInf());
+                    } else if (choose == 4) {
+                        System.out.println("Send you a cup of milk tea, maybe you will become addicted!");
+                        mt = machine.getMilkTea("PearlMilkTea");
+                        System.out.println("Your MilkTea:" + mt.getInf());
+                    } else {
+                        System.out.println("The option you entered does not exist.");
+                    }
+                }
+                else if(i==4){
+                    System.out.println("Thank you for coming and hope to see you again.");
+                    break;
+                }
+                else{
                     System.out.println("The option you entered does not exist.");
                 }
-                System.out.println("------------------- [End] --------------------");
-            }
-            else if(i==4){
-                System.out.println("Thank you for coming and hope to see you again.");
-            }
-            else{
-                System.out.println("The option you entered does not exist.");
             }
         }
         else{
