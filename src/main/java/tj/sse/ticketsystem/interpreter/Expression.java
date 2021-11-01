6fafa30dca114fa8cc7ada14d182b7db803182c4
Sherlock White <1172940427@qq.com>
2021-11-01 20:24:23 +0800

    备忘录/解释器的注释补充

--- src/main/java/tj/sse/ticketsystem/interpreter/Expression.java
+++ src/main/java/tj/sse/ticketsystem/interpreter/Expression.java
@@ -1,6 +1,8 @@
 package tj.sse.ticketsystem.interpreter;
 
-//抽象表达式类
+/**
+ * @Description 抽象表达式类
+ **/
 interface Expression {
     public boolean interpret(String info);
 }
--- src/main/java/tj/sse/ticketsystem/memento/Memento.java
+++ src/main/java/tj/sse/ticketsystem/memento/Memento.java
@@ -1,17 +1,32 @@
 package tj.sse.ticketsystem.memento;
 
-//备忘录
+/**
+ * @Classname Memento
+ * @Description 备忘录类
+ * @Author shenxingyu
+ * @Date 2021-10-10 13:00
+ * @Version 1.0
+ **/
 class Memento {
     private String state;
 
+    /**
+     * @Description 构造函数
+     **/
     public Memento(String state) {
         this.state = state;
     }
 
+    /**
+     * @Description 修改内容
+     **/
     public void setState(String state) {
         this.state = state;
     }
 
+    /**
+     * @Description 读取内容
+     **/
     public String getState() {
         return state;
     }
