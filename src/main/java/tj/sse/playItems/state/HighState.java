a7d4bec95b659c45277b1594eaaca635a614e3af
5-49 <1854025@tongji.edu.cn>
2021-11-02 18:45:28 +0800

    中介者模式

--- src/main/java/tj/sse/playItems/state/HighState.java
+++ src/main/java/tj/sse/playItems/state/HighState.java
@@ -1,15 +1,17 @@
 package tj.sse.playItems.state;
 
 //具体状态类：优秀
-public class HighState extends AbstractState {
+public class  HighState extends AbstractState {
     public HighState(AbstractState state) {
         en = state.en;
+        //设定状态名称和奖品
         stateName = "Excellent";
         gift = "Congratulations on catching the Snitch! Get a place at Hogwarts！";
         score = state.score;
     }
 
     public void checkState() {
+        //根据分数检查状态变化
         if (score < 30) {
             en.setState(new LowState(this));
         } else if (score < 50) {
--- src/main/java/tj/sse/souvenir/mediator/ConcreteMediator.java
+++ src/main/java/tj/sse/souvenir/mediator/ConcreteMediator.java
@@ -5,13 +5,16 @@ import java.util.List;
 
 //具体中介者
 public class ConcreteMediator extends Mediator {
+    //定义一个 List 来管理交易者对象
     private List<Trader> traders = new ArrayList<Trader>();
+    //注册交易者
     public void register(Trader trader) {
         if (!traders.contains(trader)) {
             traders.add(trader);
             trader.setMedium(this);
         }
     }
+    //像不是cl的交易者转发信息
     public void relay(Trader cl) {
         for (Trader ob : traders) {
             if (!ob.equals(cl)) {
